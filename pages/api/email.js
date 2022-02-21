7282024dbb3c19db4a40a48a97d72409cb92bb9c
Fraco Oxza <fraco_oxza@outlook.com>
2022-02-21 03:03:59 -0300

    Fix use undefined as an identifier

--- pages/api/email.js
+++ pages/api/email.js
@@ -3,7 +3,7 @@ import { getEmails, getEmail, addEmail } from "../../utils/db";
 export default async function handler(req, res) {
   switch (req.method) {
     case "GET": {
-      if (req.body._id === undefined) {
+      if (typeof req.body._id === "undefined") {
         let emails = await getEmails();
         res.status(200).json({ ...emails });
       } else {
