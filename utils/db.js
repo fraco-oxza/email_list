87a05c8c48cf46930aedb681039c887e63f7a745
Fraco Oxza <fraco_oxza@outlook.com>
2022-02-21 01:40:05 -0300

    First commit

---
+++ utils/db.js
@@ -0,0 +1,31 @@
+import { MongoClient, ObjectId } from "mongodb";
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri);
+
+export async function getEmails() {
+  await client.connect();
+  const db = client.db("email_list_app");
+
+  const emails = await db
+    .collection("emails")
+    .find({}, { _id: 1, name: 1, email: 1 })
+    .sort({ name: 1 })
+    .toArray();
+
+  return emails;
+}
+
+export async function getEmail(id) {
+  await client.connect();
+  const db = client.db("email_list_app");
+
+  return await db.collection("emails").findOne({ _id: new ObjectId(id) });
+}
+
+export async function addEmail({ name, email }) {
+  await client.connect();
+  const db = client.db("email_list_app");
+
+  return await db.collection("emails").insertOne({ name, email });
+}
