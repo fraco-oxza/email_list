489e8bebe1f86b8000b9866d87351a9524eb0d32
Fraco Oxza <fraco_oxza@outlook.com>
2022-02-21 01:40:05 -0300

    First commit

---
+++ pages/api/email.js
@@ -0,0 +1,23 @@
+import { getEmails, getEmail, addEmail } from "../../utils/db";
+
+export default async function handler(req, res) {
+  switch (req.method) {
+    case "GET":
+      if (req.body._id === undefined) {
+        let emails = await getEmails();
+        res.status(200).json({ ...emails });
+      } else {
+        let email = await getEmail(req.body._id);
+        res.status(200).json({ ...email });
+      }
+      break;
+    case "POST":
+      let result = addEmail(req.body);
+      console.log(JSON.stringify(req.body));
+      res.status(200).json({ result: "ok", id: result });
+
+      break;
+    default:
+      res.status(405).send();
+  }
+}
